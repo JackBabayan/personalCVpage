9538e948a7798e60e051e3b659eb84bc42f51a64
s.babayan <s.babayan@notamedia.ru>
2025-03-05 13:11:20 +0400

    add portfolio

--- src/app/pokemon-info/[id]/page.tsx
+++ src/app/pokemon-info/[id]/page.tsx
@@ -1,55 +1,49 @@
-"use client";
+"use client"
 import { useEffect, useState } from 'react';
 import Link from "next/link";
 import { useParams } from 'next/navigation';
 import {
   Box, Text, Container, Breadcrumb,
-  BreadcrumbItem, Spinner, Center
+  BreadcrumbItem,Spinner, Center
 } from '@chakra-ui/react';
 
 import { ChevronRightIcon } from "@chakra-ui/icons";
 
 export default function PokemonDetail() {
   const [data, setData] = useState(null);
-  const [id, setId] = useState<string | null>(null);
-  const params = useParams();
-
-  useEffect(() => {
-    if (params?.id) {
-      setId(params.id);  // Сохраняем id из параметров в state
-    }
-  }, [params]);
+  const { id } = useParams();
 
   useEffect(() => {
     if (!id) return;
 
     fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
       .then((res) => res.json())
-      .then((data) => setData(data))
-      .catch((err) => console.error("Ошибка при получении данных:", err));
+      .then((data) => setData(data));
   }, [id]);
 
-  if (!data) {
-    return (
-      <Container maxW="80vw" mt="20px" mb="20px">
-        <Center bg="bg.emphasized" h="100vh" w="100%">
-          <Spinner size="lg" />
-        </Center>
-      </Container>
-    );
-  }
+  if (!data) return  <Container maxW="80vw" mt="20px" mb="20px">
+    <Center bg="bg.emphasized" h="100vh" w="100%">
+      <Spinner size="lg" />
+    </Center>
+  </Container>
 
   return (
     <Container maxW="80vw" mt="20px" mb="20px">
       <Box mb="20px">
-        <Breadcrumb spacing='8px' separator={<ChevronRightIcon color='gray.500' />}>
+
+        <Breadcrumb  spacing='8px' separator={<ChevronRightIcon color='gray.500' />}>
+
           <BreadcrumbItem>
             <Link href="/">Home Page</Link>
           </BreadcrumbItem>
+
           <BreadcrumbItem>
             <Link href="/pokemon-page">Pokemon Page</Link>
           </BreadcrumbItem>
+
         </Breadcrumb>
+
+
       </Box>
 
       <Box>
